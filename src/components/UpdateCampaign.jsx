a0c040d33db56851e9345da8b0da92df846119aa
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-04-06 18:29:41 +0530

    retrieving values from  smart contract

--- src/components/UpdateCampaign.jsx
+++ src/components/UpdateCampaign.jsx
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import contractInstance from "../utils/contractInstance";
 
 const UpdateCampaign = () => {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [goal, setGoal] = useState();
+  const [endDate, setEndDate] = useState();
   const { id } = useParams();
   const getCampaignById = async () => {
     const campaign = await contractInstance.methods
       .getCampaignDetails(id)
       .call();
     console.log("Campaign details:", campaign);
+    setTitle(campaign.title);
+    setDescription(campaign.description);
+    setImageUrl(campaign.imageUrl);
+    setGoal(campaign.target);
+    setEndDate(campaign.deadline);
   };
   useEffect(() => {
     getCampaignById();
@@ -26,6 +36,8 @@ const UpdateCampaign = () => {
             id="title"
             type="text"
             placeholder="Enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -39,6 +51,8 @@ const UpdateCampaign = () => {
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="description"
             placeholder="Enter description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -52,6 +66,8 @@ const UpdateCampaign = () => {
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="description"
             placeholder="Enter Image URL"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -63,6 +79,8 @@ const UpdateCampaign = () => {
             id="goal"
             type="number"
             placeholder="Enter goal"
+            value={goal}
+            onClick={(e) => setGoal(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -77,6 +95,8 @@ const UpdateCampaign = () => {
             id="endDate"
             type="date"
             placeholder="Enter end date"
+            value={endDate}
+            onClick={(e) => setEndDate(e.target.value)}
           />
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
